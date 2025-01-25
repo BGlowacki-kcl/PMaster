e1965b2c2febfabc5ef6ebda3def7e0fcb4128c1
Bartosz <bglowacki.kcl@gmail.com>
2025-01-25 22:58:48 +0000

    Init vite frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 const app = express();
 dotenv.config();
@@ -9,5 +10,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT, () => {
+    connectDB();
     console.log(`Server is running on port http://localhost:${process.env.PORT}`);
 })
