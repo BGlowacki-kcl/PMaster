a996b0ae16fc4c11428de01bf80ddb26149676e1
Bartosz <bglowacki.kcl@gmail.com>
2025-03-06 23:46:14 +0000

    final responsiveness touches

--- frontend/src/components/FeatureCard.jsx
+++ frontend/src/components/FeatureCard.jsx
@@ -3,15 +3,24 @@ import React from 'react'
 
 const FeatureCard = ({ img, text, title }) => {
   return (
-    <Card className='hover:animate-fade-in-scale h-full p-3' sx={{ backgroundColor: "#F8C794;", borderRadius: "14px"}}>
-        <CardMedia>
-            <img src={img} className="w-auto h-[150px] md:h-[20vw] xl:h-[15vw] shadow-md rounded-lg border-2 border-secondarysky2 ml-auto mr-auto" alt="schedule" />
-        </CardMedia>
+    <Card 
+      className='hover:animate-fade-in-scale flex flex-col justify-between h-full min-h-[350px] sm:min-h-[400px] p-3' 
+      sx={{ backgroundColor: "#F8C794;", borderRadius: "14px" }}
+    >
+      <CardMedia>
+        <img 
+          src={img} 
+          className="w-auto h-[150px] md:h-[20vw] xl:h-[15vw] shadow-md rounded-lg border-2 border-secondarysky2 mx-auto" 
+          alt={title} 
+        />
+      </CardMedia>
 
-        <CardContent>
-            <p className="font-font3 text-xl text-center" > {title} </p>
-            <p className='font-font4 mt-3 text-justify text-[3vw] md:text-[2vw] lg:text-[1.5vw]'> {text} </p>
-        </CardContent>
+      <CardContent className="flex flex-col flex-grow">
+        <p className="font-font3 text-xl text-center"> {title} </p>
+        <p className='font-font4 mt-3 text-justify text-[4vw] sm:text-[3vw] md:text-[2vw] lg:text-[1.5vw] flex-grow'>
+          {text}
+        </p>
+      </CardContent>
     </Card>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = forwardRef((props, ref, showNotification) => {
 
             <button
               onClick={showWaitlist}
-              className="z-10 rounded-full min-w-[16px] cursor-pointer hover:shadow hover:bg-primarywood1 ml-auto mr-20 h-12 text-bold font-font2 text-xl px-6 bg-primarywood1/80"
+              className="z-10 rounded-full min-w-[180px] cursor-pointer hover:shadow hover:bg-primarywood1 ml-auto mr-20 h-12 text-bold font-font2 text-xl px-6 bg-primarywood1/80"
             >
               Waitlist!
             </button>
