bfc4e54c03a07353f385e472404b6a34e181e8b4
Bartosz <bglowacki.kcl@gmail.com>
2025-03-06 21:14:46 +0000

    Specify PORT to backend request

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'https://www.krono.it.com',
+        target: 'https://www.krono.it.com:5000',
       }
     }
   },
